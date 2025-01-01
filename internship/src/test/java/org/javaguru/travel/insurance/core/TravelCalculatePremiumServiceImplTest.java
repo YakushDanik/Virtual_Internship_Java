4d7bd7c2d8c9194f7f7e06fc906339445a75e085
YakushDanik <yakushdanik9@gmail.com>
2025-01-01 15:07:18 +0300

    task_14 and task_15 done

--- internship/src/test/java/org/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImplTest.java
+++ internship/src/test/java/org/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImplTest.java
@@ -2,6 +2,7 @@ package org.javaguru.travel.insurance.core;
 
 import org.javaguru.travel.insurance.rest.TravelCalculatePremiumRequest;
 import org.javaguru.travel.insurance.rest.TravelCalculatePremiumResponse;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Date;
@@ -11,25 +12,78 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class TravelCalculatePremiumServiceImplTest {
 
-    private final TravelCalculatePremiumServiceImpl service = new TravelCalculatePremiumServiceImpl();
-    @Test
-    public void deleteMe() {
+    private TravelCalculatePremiumServiceImpl service;
 
+    @BeforeEach
+    public void setUp() {
+        service = new TravelCalculatePremiumServiceImpl();
+    }
+
+    private TravelCalculatePremiumRequest createRequest(String firstName, String lastName, Date dateFrom, Date dateTo) {
+        return new TravelCalculatePremiumRequest(firstName, lastName, dateFrom, dateTo);
     }
 
     @Test
-    public void calculatePremiumTest(){
+    public void calculatePremiumNotNullTest() {
         String firstName = "Danila";
         String lastName = "Yakush";
         Date dateFrom = new Date();
         Date dateTo = new Date(dateFrom.getTime() + 100000);
-        TravelCalculatePremiumRequest request = new TravelCalculatePremiumRequest(firstName, lastName, dateFrom, dateTo);
+        TravelCalculatePremiumRequest request = createRequest(firstName, lastName, dateFrom, dateTo);
+
         TravelCalculatePremiumResponse response = service.calculatePremium(request);
+
         assertNotNull(response);
-        assertEquals(firstName, response.getPersonFirstName());
-        assertEquals(lastName, response.getPersonLastName());
-        assertEquals(dateFrom, response.getAgreementDateFrom());
-        assertEquals(dateTo, response.getAgreementDateTo());
     }
 
+    @Test
+    public void calculatePremiumFirstNameTest() {
+        String firstName = "Danila";
+        String lastName = "Yakush";
+        Date dateFrom = new Date();
+        Date dateTo = new Date(dateFrom.getTime() + 100000);
+        TravelCalculatePremiumRequest request = createRequest(firstName, lastName, dateFrom, dateTo);
+
+        TravelCalculatePremiumResponse response = service.calculatePremium(request);
+
+        assertEquals(firstName, response.getPersonFirstName());
+    }
+
+    @Test
+    public void calculatePremiumLastNameTest() {
+        String firstName = "Danila";
+        String lastName = "Yakush";
+        Date dateFrom = new Date();
+        Date dateTo = new Date(dateFrom.getTime() + 100000);
+        TravelCalculatePremiumRequest request = createRequest(firstName, lastName, dateFrom, dateTo);
+
+        TravelCalculatePremiumResponse response = service.calculatePremium(request);
+
+        assertEquals(lastName, response.getPersonLastName());
+    }
+
+    @Test
+    public void calculatePremiumDateFromTest() {
+        String firstName = "Danila";
+        String lastName = "Yakush";
+        Date dateFrom = new Date();
+        Date dateTo = new Date(dateFrom.getTime() + 100000);
+        TravelCalculatePremiumRequest request = createRequest(firstName, lastName, dateFrom, dateTo);
+
+        TravelCalculatePremiumResponse response = service.calculatePremium(request);
+
+        assertEquals(dateFrom, response.getAgreementDateFrom());
+    }
+
+    @Test
+    public void calculatePremiumDateToTest() {
+        String firstName = "Danila";
+        String lastName = "Yakush";
+        Date dateFrom = new Date();
+        Date dateTo = new Date(dateFrom.getTime() + 100000);
+        TravelCalculatePremiumRequest request = createRequest(firstName, lastName, dateFrom, dateTo);
+
+        TravelCalculatePremiumResponse response = service.calculatePremium(request);
+        assertEquals(dateTo, response.getAgreementDateTo());
+    }
 }
